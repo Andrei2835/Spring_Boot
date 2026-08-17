3d8aeb30e8f025c597ae4baf604fe9500489cf12
Andrey <fedorov_a_v_85@mail.ru>
2026-08-17 16:21:19 +0300

    реализация задачи 3.1.1

--- src/main/java/crudnew/web/spring_boot/spring_boot_rest/dao/UserDao.java
+++ src/main/java/crudnew/web/spring_boot/spring_boot_rest/dao/UserDao.java
@@ -1,14 +1,18 @@
 package crudnew.web.spring_boot.spring_boot_rest.dao;
 
-
 import crudnew.web.spring_boot.spring_boot_rest.model.User;
 
 import java.util.List;
 
 public interface UserDao {
+
     List<User> findAll();
+
     User findById(Long id);
+
     void save(User user);
+
     void update(User user);
+
     void deleteById(Long id);
 }
--- src/main/java/crudnew/web/spring_boot/spring_boot_rest/model/User.java
+++ src/main/java/crudnew/web/spring_boot/spring_boot_rest/model/User.java
@@ -1,6 +1,12 @@
 package crudnew.web.spring_boot.spring_boot_rest.model;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.util.Objects;
 
